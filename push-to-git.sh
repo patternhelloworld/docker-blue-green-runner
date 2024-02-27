dd969101d573946d66a0e7fe80add9bb9b0f5668
Andrew-Kang-G <studypurpose@naver.com>
2024-02-27 16:50:38 +0900

    test2

--- push-to-git.sh
+++ push-to-git.sh
@@ -14,6 +14,7 @@ _main() {
   local state_for_push=${state}
 
   git_image_push_to_host=$(get_value_from_env "GIT_IMAGE_PUSH_TO_HOST")
+  git_image_push_to_pathname=$(get_value_from_env "GIT_IMAGE_PUSH_TO_PATHNAME")
   git_token_image_push_to_username=$(get_value_from_env "GIT_TOKEN_IMAGE_PUSH_TO_USERNAME")
   git_token_image_push_to_password=$(get_value_from_env "GIT_TOKEN_IMAGE_PUSH_TO_PASSWORD")
 
@@ -21,12 +22,17 @@ _main() {
   echo "[NOTICE] Attempt to log in to the Registry."
   docker_login_with_params ${git_token_image_push_to_username} ${git_token_image_push_to_password} ${git_image_push_to_host}
 
-  echo "[NOTICE] Prepare a current version and push"
-  docker tag ${project_name}:${state_for_push} ${git_image_push_to_host}/${project_name}
-  docker push ${git_image_push_to_host}/${project_name}
+  echo "[NOTICE] Prepare current versions of the App and Nginx and push them."
 
-  docker tag ${project_name}-nginx:latest ${git_image_push_to_host}/${project_name}:nginx
-  docker push ${git_image_push_to_host}/${project_name}:nginx
+  echo "[DEBUG] Run : docker tag ${project_name}:${state_for_push} ${git_image_push_to_host}/${git_image_push_to_pathname}"
+  docker tag ${project_name}:${state_for_push} ${git_image_push_to_host}/${git_image_push_to_pathname}/app
+  echo "[DEBUG] Run : docker push ${git_image_push_to_host}/${git_image_push_to_pathname}"
+  docker push ${git_image_push_to_host}/${git_image_push_to_pathname}
+
+  echo "[DEBUG] Run : docker tag ${project_name}-nginx:latest ${git_image_push_to_host}/${git_image_push_to_pathname}/nginx"
+  docker tag ${project_name}-nginx:latest ${git_image_push_to_host}/${git_image_push_to_pathname}/nginx
+  echo "[DEBUG] Run : docker push ${git_image_push_to_host}/${git_image_push_to_pathname}/nginx}"
+  docker push ${git_image_push_to_host}/${git_image_push_to_pathname}/nginx
 
 }
 
