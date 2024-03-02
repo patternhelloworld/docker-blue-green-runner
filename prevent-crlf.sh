d21efac4673553d0a1430a95027dc8f34dfeb3c9
Andrew-Kang-G <studypurpose@naver.com>
2024-03-02 16:06:09 +0900

    fix : modify 'prevent-crlf.sh' for supressing WIN WSL2 CRLF errors

--- prevent-crlf.sh
+++ prevent-crlf.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sudo sed -i -e "s/\r$//g" $(basename $0) || sed -i -e "s/\r$//g" $(basename $0)
-sed -i -e 's/\r$//' *.sh
-sed -i -e 's/\r$//' ./.docker/nginx/logrotate
-sed -i -e 's/\r$//' ./.docker/nginx/nginx.service
-sed -i -e 's/\r$//' ./.docker/nginx/entrypoint.sh
-sed -i -e 's/\r$//' .env .env.example.local .env.example.real .env.* *.yml  || echo "[NOTICE] Performed CRLF line ending inspection. There are no issues with the non-existent files."
+
+# Attempt to remove carriage return characters from the script itself with sudo; if that fails, try without sudo.
+find . -name "*.sh" -exec sed -i -e 's/\r$//' {} \;
+
+# Remove carriage return characters from all shell script files in the current directory.
+find ./.docker/nginx -type f \( -name 'logrotate' -o -name 'nginx.service' -o -name 'entrypoint.sh' \) -exec sed -i -e 's/\r$//' {} \;
+
+# Remove carriage return characters from specific configuration files.
+find . -type f \( -name '.env' -o -name '.env.example.local' -o -name '.env.example.real' -o -name '.env.*' -o -name '*.yml' \) -exec sed -i -e 's/\r$//' {} \; || echo "[NOTICE] Performed CRLF line ending inspection. There are no issues with the non-existent files."
