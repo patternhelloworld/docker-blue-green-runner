4c6132852036d8c34975a09df4b81a2fe5bcbadb
Andrew-Kang-G <studypurpose@naver.com>
2024-04-26 18:00:52 +0900

    fix : set a selective parameter on rollback.sh

--- rollback.sh
+++ rollback.sh
@@ -11,40 +11,31 @@ source ./use-app.sh
 
 cache_global_vars
 
-# 1) App rollback
-echo "[NOTICE] Change the previous app image to the ${new_state} image for rollback."
-docker tag ${project_name}:previous ${project_name}:${new_state} || echo "[NOTICE] ${project_name}:previous image does NOT exist."
-
-sleep 2
-
-echo "[NOTICE] Down & Up ${new_state} container."
-docker-compose -f docker-compose-${project_name}-${app_env}.yml stop ${project_name}-${new_state} || echo "[NOTICE] The ${new_state} Container has been stopped, if exists."
-docker-compose -f docker-compose-${project_name}-${app_env}.yml rm -f ${project_name}-${new_state} || echo "[NOTICE] The ${new_state} Container has been removed, if exists."
-docker-compose -f docker-compose-${project_name}-${app_env}.yml up -d ${project_name}-${new_state}
-
-echo "[NOTICE] Wait until the ${new_state} container is fully up."
-if [[ $(check_availability_inside_container ${new_state} 60 5 | tail -n 1) != 'true' ]]; then
-  echo "[ERROR] Failed to rollback to the ${new_state} container." && exit 1
+with_nginx="${1:-}"
+if [[ "$with_nginx" == "1" ]]; then
+    nginx_restart=true
+else
+    nginx_restart=false
 fi
 
-# 2) Nginx rollback
-if [ ${nginx_restart} = "true" ]; then
+# Nginx Rollback
+if [[ ${nginx_restart} == 'true' ]]; then
   echo "[NOTICE] Change the 'previous' tagged Nginx image to the 'latest' tagged image."
   docker tag ${project_name}-nginx:previous ${project_name}-nginx:latest || echo "[NOTICE] ${project_name}-nginx:previous image does NOT exist."
 
   sleep 2
 
-  echo "[NOTICE] Up the Nginx latest tagged container"
-  docker-compose -f docker-compose-${project_name}-nginx.yml up -d ${project_name}-nginx
+  echo "[NOTICE] Run 'emergency-nginx-restart.sh'"
+  bash emergency-nginx-restart.sh
 fi
 
 ./activate.sh ${new_state} ${state} ${new_upstream} ${consul_key_value_store}
 
 
 if [[ ${orchestration_type} != 'stack' ]]; then
-  echo "[NOTICE] Shut down the existing ${state} container."
+  echo "[NOTICE] Stop the previous ${state} container."
   docker-compose -f docker-${orchestration_type}-${project_name}-${app_env}.yml stop ${project_name}-${state}
 else
-  echo "[NOTICE] Remove the existing ${state} stack."
+  echo "[NOTICE] Remove the previous ${state} stack."
   docker stack rm ${project_name}-${state}
 fi
