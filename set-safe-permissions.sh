4c51c4c381a56280c91a257a8ec5ebd726c7ad90
Andrew-Kang-G <studypurpose@naver.com>
2024-06-21 09:40:26 +0900

    fix : a bug command on 'set-safe-permissions.sh'

--- set-safe-permissions.sh
+++ set-safe-permissions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo sed -i -e "s/\r$//g" $(basename $0) || sed -i -e "s/\r$//g" $(basename $0)
 sudo chmod 770 *
-sudo chmod tests/*
+sudo chmod 770 .env.*
+sudo chmod 770 tests/*
 sudo chmod -R 770 .docker/nginx
 # This is temporary. You should set your SSLs to be such as 644 or 640 on your SSL folder.
 sudo chmod -R 770 .docker/ssl
